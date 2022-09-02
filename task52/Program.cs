d4b99b2888b25bc8f3f9add2567f9f19b63b4787
Nancy <nastyshazoloto@mail.ru>
2022-09-02 19:17:16 +0300

    task52

---
+++ task52/Program.cs
@@ -0,0 +1,16 @@
+﻿static void Main(string[] args)
+{
+    int[,] mas = new int[3, 3] { { 1, 2, 3 }, { 3, 5, 7 }, { 5, 6, 1 } };
+
+    for (int i = 0; i < 3; i++)
+    {
+        int srAr = 0;
+        int ColChetn = 0;
+        for (int t = 0; t < 3; t++)
+        {
+            srAr += mas[t, i];
+            ColChetn += (mas[t, i] % 2 == 0) ? 1 : 0;
+        }
+        Console.WriteLine($"Cреднее арифметическое элементов столбца {i + 1} = {(float)srAr / 3}");
+    }
+}
